23c06505981ee0eeed87446edcb2b4e3ab6c9459
Igor Macedo <igormacedo94@yahoo.com.br>
2018-08-23 12:47:04 -0300

    implemented RAM module

--- RAM/source/ram.sv
+++ RAM/source/ram.sv
@@ -3,10 +3,28 @@ module ram(
     data_out,
     addr,
     enable,
-    read_write_en
+    clk,
+    write_en
 );
 
-input enable, read_write_en;
+input enable, write_en, clk;
+input[15:0] data_in;
+input[7:0] addr;
+output reg[15:0] data_out;
 
+reg[15:0] memory[0:255];
+
+always @(posedge clk) begin
+    if (enable == 1) begin
+        if (write_en == 1) begin
+            memory[addr] <= data_in;
+            data_out <= data_in;
+        end else begin
+            data_out <= memory[addr];
+        end
+    end else begin
+        data_out <= 15'bz;
+    end
+end
 
 endmodule
--- RAM/tests/ramtest.sv
+++ RAM/tests/ramtest.sv
@@ -1,1 +1,54 @@
 module ramtb;
+
+reg[15:0] din;
+reg[7:0] addr;
+reg enable, write_en, clk;
+wire[15:0] dout;
+
+ram rd(
+    .data_in(din),
+    .data_out(dout),
+    .addr(addr),
+    .clk(clk),
+    .enable(enable),
+    .write_en(write_en)
+);
+
+integer i;
+
+initial begin
+    din = 0;
+    addr = 0;
+    enable = 1;
+    write_en = 1;
+    clk = 0;
+
+    for (i = 0; i < 255; i = i + 1) begin
+        rd.memory[i] = i;
+    end
+    
+end
+
+always #1 clk <= ~clk;
+
+initial begin
+    $dumpfile("build/wave.vcd");
+    $dumpvars(0, ramtb);
+
+    for (i = 0; i < 256; i = i + 1) begin
+        din <= i*2;
+        addr <= i;
+        if (i == 250) begin
+            enable <= ~enable;
+        end
+
+        if (i == 150) begin
+            write_en <= ~write_en;
+        end
+        #2;
+    end
+
+    $finish;
+end
+
+endmodule
